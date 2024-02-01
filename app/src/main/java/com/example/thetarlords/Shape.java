5746c6c1ab29da320e30aeca534341dbd8aa08b0
Lucy <124856770+gjiyun@users.noreply.github.com>
2024-01-31 20:00:03 -0700

    Update Shape.java

--- app/src/main/java/com/example/thetarlords/Shape.java
+++ app/src/main/java/com/example/thetarlords/Shape.java
@@ -3,5 +3,5 @@ package com.example.thetarlords;
 public abstract class Shape {
     private int x;
     private int y;
-    String color = "blue";
+    String color = "$PUT_YOUR_CHOICE_OF_COLOR";
 }
